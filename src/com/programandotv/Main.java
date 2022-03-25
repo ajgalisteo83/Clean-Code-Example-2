c86a0554284014badcaaecbe5e74df68637d6a96
Antonio Galisteo <antonio.galisteo@thoughtworks.com>
2022-03-25 11:09:46 -0500

    Clean Code Example 2

---
+++ src/com/programandotv/Main.java
@@ -0,0 +1,13 @@
+package com.programandotv;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        float base = 8.0f;
+        float height = 15.2f;
+
+        Rectangle rectangle = new Rectangle();
+        System.out.println("The area is: " + rectangle.area(base,height) + " cm2");
+    }
+}
---
+++ src/com/programandotv/Rectangle.java
@@ -0,0 +1,8 @@
+package com.programandotv;
+
+public class Rectangle {
+
+    public float area(float base, float height) {
+        return base*height;
+    }
+}
